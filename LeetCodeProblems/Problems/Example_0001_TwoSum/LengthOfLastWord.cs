3d224e97d6e18ac650faa1b2b2fed540d07b517e
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2026-04-20 15:15:36 +0100

    Obtain the last word length from string received

--- LeetCodeProblems/Problems/Example_0001_TwoSum/LengthOfLastWord.cs
+++ LeetCodeProblems/Problems/Example_0001_TwoSum/LengthOfLastWord.cs
@@ -2,16 +2,36 @@ public class LengthOfLastWord : IProblem
 {
     public string Name => "LengthOfLastWord";
 
+    public readonly string[] testCases = new[]
+    {
+        "Hello World",
+        "   fly me   to   the moon  ",
+        "luffy is still joyboy",
+        "",
+        "a",
+        "a ",
+        " a"
+    };
+
     public void Run()
     {
         Console.WriteLine("Running Length of Last Word example...");
-        string s = "Hello World";
-        int res = LengthOfLastWordFunc(s);
-        Console.WriteLine($"Result: {res}");
+        foreach (string s in testCases)
+        {
+            int res = LengthOfLastWordFunc(s);
+            Console.WriteLine($"String value : {s} | Result: {res}");
+        }
     }
 
     private int LengthOfLastWordFunc(string s)
     {
+        if (string.IsNullOrEmpty(s))
+        {
+            return 0;
+        }
 
+        ReadOnlySpan<char> span = s.AsSpan().TrimEnd();
+        int lastSpace = span.LastIndexOf(' ');
+        return lastSpace >= 0 ? span.Length - lastSpace - 1 : span.Length;
     }
 }
